74b611fb69f31dfa5f068a331f44700f0e9def77
Óscar Grande <oscargrgm@gmail.com>
2021-01-11 15:05:21 +0100

    Created RefreshDataWork.

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -17,11 +17,31 @@
 
 package com.example.android.devbyteviewer.work
 
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+
 // TODO (01) Create the RefreshDataWorker class, extend it from CoroutineWorker, and
-// pass in a Context and WorkerParams.
+//  pass in a Context and WorkerParams.
+class RefreshDataWorker(
+    appContext: Context,
+    params: WorkerParameters
+) : CoroutineWorker(appContext, params) {
 
-// TODO (02) Override the required doWork() method, and create variables for the
-// database and the repository.
-
-// TODO (03) Inside doWork(), in a try-catch block, refresh the videos, and
-// use Payload() to return SUCCESS or RETRY result.
+    // TODO (02) Override the required doWork() method, and create variables for the
+    //  database and the repository.
+    // TODO (03) Inside doWork(), in a try-catch block, refresh the videos, and
+    //  use Payload() to return SUCCESS or RETRY result.
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (ex: Throwable) {
+            Result.retry()
+        }
+    }
+}
